5534f818170f3e6331330ec18fe6c18656d0b7ba
Alex <ag2719@ic.ac.uk>
2023-04-29 01:08:32 +0100

    Implement main functionality of bezier curve processing, modularize _get_label_from_stroke_file function.

--- iam_extraction.py
+++ iam_extraction.py
@@ -11,11 +11,13 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
+import math
 import os
 import xml.etree.ElementTree as ET
 import numpy as np
+import matplotlib.pyplot as plt
 
-from bezier import Bezier
+import scipy.optimize
 
 
 LINE_STROKES_DATA_DIR = Path("../datasets/IAM/lineStrokes")
@@ -34,6 +36,70 @@ class StrokeData:
     pen_ups: list[int]  # 0 if pen is down, 1 if pen is up (end of stroke).
 
 
+def _get_line_from_labels_file(
+    labels_file: Path, line_idx: int
+) -> Optional[str]:
+    """Gets the line label from the labels file.
+
+    Args:
+        labels_file (Path): The labels file path.
+        line_idx (int): The line index.
+
+    Returns:
+        The line label. None if the line label is not found.
+
+    Raises:
+        ValueError: If the labels file is invalid.
+        FileNotFoundError: If the labels file is not found.
+        ValueError: If the labels file is not a file.
+        ValueError: If the line label index is not a number.
+    """
+    # Check if the labels file is valid.
+    if not isinstance(labels_file, Path):
+        raise ValueError(
+            f"Invalid labels file: {labels_file}. Must be an instance of {Path}."
+        )
+
+    # Check if the labels file exists.
+    if not labels_file.exists():
+        raise FileNotFoundError(f"Labels file not found: {labels_file}.")
+
+    # Check if the labels file is a file.
+    if not labels_file.is_file():
+        raise ValueError(f"Labels file is not a file: {labels_file}.")
+
+    # Check if the line label index is valid.
+    if not isinstance(line_idx, int):
+        raise ValueError(
+            f"Invalid line label index: {line_idx}. Must be an integer."
+        )
+
+    # Get the line label from the labels file.
+    with open(labels_file, "r", encoding="utf-8") as file:
+        # The index of the line where the line labels start (CSR line).
+        labels_start_idx = 0
+
+        for i, line in enumerate(file):
+            # Check if we have found CSR line and save the index.
+            if line.startswith("CSR:"):
+                labels_start_idx = i
+
+            if (
+                labels_start_idx > 0  # Ensure we have found the CSR line.
+                and i
+                == labels_start_idx
+                + line_idx
+                + 1  # Add 1 to skip the blank line after the CSR line.
+            ):
+                # Only keep the characters that are in the IAM dataset.
+                return "".join(
+                    [char for char in line if char.isalnum() or char == " "]
+                )
+
+    # If we have not found the line label, return None.
+    return None
+
+
 def _get_label_from_stroke_file(stroke_file: Path) -> Optional[str]:
     """Gets the line label from the stroke file.
 
@@ -48,7 +114,6 @@ def _get_label_from_stroke_file(stroke_file: Path) -> Optional[str]:
         FileNotFoundError: If the stroke file is not found.
         ValueError: If the stroke file is not an XML file.
         ValueError: If the line label index is not a number.
-        ValueError: If the line label index is not found.
     """
     # Check if the stroke file is valid.
     if not isinstance(stroke_file, Path):
@@ -88,35 +153,12 @@ def _get_label_from_stroke_file(stroke_file: Path) -> Optional[str]:
         / sub_labels_dir
         / f"{labels_file_name}.txt"
     )
-    assert labels_file.is_file(), f"Label file not found: {labels_file}."
 
-    # Get the line label from the label file.
-    with open(labels_file, "r", encoding="utf-8") as labels_file:
-        # The index of the line where the line labels start (CSR line).
-        labels_start_idx = 0
-
-        for i, line in enumerate(labels_file):
-            # Check if we have found CSR line and save the index.
-            if line.startswith("CSR:"):
-                labels_start_idx = i
-
-            if (
-                labels_start_idx > 0  # Ensure we have found the CSR line.
-                and i
-                == labels_start_idx
-                + line_label_idx
-                + 1  # Add 1 to skip the blank line after the CSR line.
-            ):
-                # Only keep the characters that are in the IAM dataset.
-                return "".join(
-                    [char for char in line if char.isalnum() or char == " "]
-                )
-
-    # If we have not found the line label, return None.
-    return None
+    # Get the line label from the labels file.
+    return _get_line_from_labels_file(labels_file, line_label_idx)
 
 
-def _parse_stroke_element(stroke: ET.Element,) -> StrokeData:
+def _parse_stroke_element(stroke_elem: ET.Element,) -> StrokeData:
     """Parses the stroke element.
 
     Args:
@@ -129,9 +171,9 @@ def _parse_stroke_element(stroke: ET.Element,) -> StrokeData:
         ValueError: If the stroke element is invalid.
     """
     # Check if the stroke element is valid.
-    if not isinstance(stroke, ET.Element):
+    if not isinstance(stroke_elem, ET.Element):
         raise ValueError(
-            f"Invalid stroke ET element: {stroke}. Must be an instance of {ET.Element}."
+            f"Invalid stroke ET element: {stroke_elem}. Must be an instance of {ET.Element}."
         )
 
     x_points = []
@@ -141,7 +183,7 @@ def _parse_stroke_element(stroke: ET.Element,) -> StrokeData:
     fst_timestamp = 0
 
     # Go through each point in the stroke.
-    for i, point in enumerate(stroke.findall("Point")):
+    for i, point in enumerate(stroke_elem.findall("Point")):
         x_points.append(float(point.attrib["x"]))
         y_points.append(float(point.attrib["y"]))
 
@@ -152,7 +194,7 @@ def _parse_stroke_element(stroke: ET.Element,) -> StrokeData:
         else:
             time_stamps.append(float(point.attrib["time"]) - fst_timestamp)
 
-        if i == len(stroke.findall("Point")) - 1:
+        if i == len(stroke_elem.findall("Point")) - 1:
             # If this is the last point in the stroke, the pen is up.
             pen_ups.append(1)
         else:
@@ -162,7 +204,7 @@ def _parse_stroke_element(stroke: ET.Element,) -> StrokeData:
     assert (
         len(x_points) == len(y_points) == len(time_stamps) == len(pen_ups)
     ), (
-        f"Invalid stroke element: {stroke}. "
+        f"Invalid stroke element: {stroke_elem}. "
         "The number of x points, y points, time stamps, and pen ups must be equal."
     )
 
@@ -247,40 +289,80 @@ def _get_data_from_stroke_file(stroke_file: Path,) -> list[StrokeData]:
     return strokes
 
 
-def _process_bezier_curve(
-    stroke: StrokeData, num_points: int = 100
-) -> StrokeData:
-    """Process the Bezier curve for the given stroke.
-
-    Args:
-        stroke (StrokeData): The stroke data.
-        num_points (int): The number of points to sample along the Bezier curve.
-
-    Returns:
-        The processed stroke data containing the Bezier curve points.
-    """
+# TODO: Clean up this function and code in general. Add type hints, docstrings, etc.
+def _process_bezier_curve(stroke: StrokeData) -> np.ndarray:
     if len(stroke.x_points) < 2:
         return stroke
 
     points = np.column_stack((stroke.x_points, stroke.y_points))
-    bezier = Bezier(points)
-    t_values = np.linspace(0, 1, num_points)
-    bezier_points = bezier.get_points(t_values)
 
-    x_points = list(bezier_points[:, 0])
-    y_points = list(bezier_points[:, 1])
-    time_stamps = np.interp(
-        t_values,
-        np.linspace(0, 1, len(stroke.time_stamps)),
-        stroke.time_stamps,
-    ).tolist()
+    # Define the Bézier curve as a function of the parameter s and coefficients (alpha, beta)
+    def bezier_curve(
+        s: float, alpha: np.ndarray, beta: np.ndarray
+    ) -> np.ndarray:
+        x = alpha[0] + alpha[1] * s + alpha[2] * s ** 2 + alpha[3] * s ** 3
+        y = beta[0] + beta[1] * s + beta[2] * s ** 2 + beta[3] * s ** 3
+        return np.array([x, y])
 
-    return StrokeData(x_points, y_points, time_stamps, stroke.end_stroke)
+    # Define the objective function to minimize the sum of squared errors (SSE)
+    def objective_function(
+        coefficients: np.ndarray, points: np.ndarray
+    ) -> float:
+        alpha, beta = coefficients[:4], coefficients[4:]
+        s_values = np.linspace(0, 1, len(points))
+        curve_points = np.array(
+            [bezier_curve(s, alpha, beta) for s in s_values]
+        )
+        errors = curve_points - points
+        return np.sum(errors ** 2)
+
+    # Find the coefficients that minimize the SSE
+    initial_coefficients = np.ones(8)
+    result = scipy.optimize.minimize(
+        objective_function, initial_coefficients, args=(points,)
+    )
+    alpha, beta = result.x[:4], result.x[4:]
+
+    # Get the vectors between the end points
+    dx_dy = np.array([alpha[3], beta[3]])
+
+    # Get the distances between the end points and the control points
+    d1 = np.linalg.norm(np.array([alpha[1], beta[1]]))
+    d2 = np.linalg.norm(np.array([alpha[2], beta[2]]))
+    d1 /= np.linalg.norm(dx_dy)
+    d2 /= np.linalg.norm(dx_dy)
+
+    # Get the angles between control points and endpoints in radians
+    a1 = math.atan2(beta[1], alpha[1])
+    a2 = math.atan2(beta[2], alpha[2])
+
+    # Calculate the time coefficients
+    time_range = np.max(stroke.time_stamps) - np.min(stroke.time_stamps)
+    gamma = np.array(
+        [time_range * alpha[1], time_range * alpha[2], time_range * alpha[3]]
+    )
+
+    # Boolean value indicating whether this is a pen-up or pen-down curve
+    p = int(stroke.pen_ups[-1])
+
+    result = np.array([*dx_dy, d1, d2, a1, a2, *gamma, p])
+    print(result.shape)
+    print(result)
+
+    # Plot the result
+    s_values = np.linspace(0, 1, len(points))
+    curve_points = np.array([bezier_curve(s, alpha, beta) for s in s_values])
+    plt.plot(points[:, 0], points[:, 1], "o")
+    plt.plot(curve_points[:, 0], curve_points[:, 1], "-")
+    plt.show()
+
+    return np.array([*dx_dy, d1, d2, a1, a2, *gamma, p])
 
 
+# TODO: Finish implementing this function.
 def extract_all_data() -> None:
-    all_stroke_data = []
     all_labels = []
+    i = 0
 
     # Go through each directory in the line strokes data directory.
     for root, _, stroke_files in os.walk(LINE_STROKES_DATA_DIR):
@@ -308,9 +390,16 @@ def extract_all_data() -> None:
             # Get the data from the stroke file.
             strokes = _get_data_from_stroke_file(stroke_file)
 
-            bezier_curves = [
-                _process_bezier_curve(stroke) for stroke in strokes
-            ]
+            _process_bezier_curve(strokes[0])
+            # plot the strokes[0]
+            plt.plot(strokes[0].x_points, strokes[0].y_points)
+            plt.show()
+
+            print(label)
+
+            # bezier_curves = [
+            #     _process_bezier_curve(stroke) for stroke in strokes
+            # ]
 
 
 extract_all_data()
